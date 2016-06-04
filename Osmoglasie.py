41bcad268cdb5084e6abe63d74f2406bf7798de7
Евсеев Андрей <and@and-laptop.(none)>
2016-06-04 15:45:59 +0300

    Начальная версия. Принимает на вход текст, разбитый на логические строки (без ударений). Может разметить на 8 глас (без акцентов, только начальный оборот).

---
+++ Osmoglasie.py
@@ -0,0 +1,137 @@
+﻿#-------------------------------------------------------------------------------
+# Name:        модуль1
+# Purpose:
+#
+# Author:      Танюшка
+#
+# Created:     15.05.2016
+# Copyright:   (c) Танюшка 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+from SyllableTree import *
+
+"""
+Разметить текст на глас указанного жанра песнопений (тропарный и т.д.)
+"""
+def Markup(text, glas, type):
+    # Разбиваем на логические строки (по / и //)
+    ls = GetLogicalStrings(text)
+
+    # Получаем шаблон разметки на нужный глас
+    markupper = GetGlasTemplate(glas, type)
+
+    # Размечаем текст
+    m_text = markupper.Markup(ls)
+
+    # Возвращаем результат в виде текста с разметкой
+    return str(m_text)
+
+"""
+Разбить текст на логические строки
+"""
+def GetLogicalStrings(text):
+    k = text.split('//')
+    if len(k) != 2:
+        raise LogicalStringsException('Некорректное конечное колено')
+
+    if '/' in k[1]:
+        raise LogicalStringsException('Обозначено конечным не последнее колено')
+
+    lines = k[0].split("/")
+
+    return list(map(str.strip, lines + [k[1]]))
+
+def GetGlasTemplate(glas, type):
+    if glas == 8 and type == "тропарь":
+        return Glas8_Template()
+
+    raise NotImplementedError()
+
+class Glas8_Template:
+    def CreateSchema(self):
+        return Glas8_Schema()
+
+    """
+    Размечает логические строки на глас
+    """
+    def Markup(self, strings):
+        result = []
+        schema = self.CreateSchema()
+        # количество неразмеченных строк
+        ost = len(strings)
+        # размечаем все строки, кроме последней
+        for s in strings[:-1]:
+            # переход на следующее колено
+            schema.Next(ost)
+            ost = ost-1
+            result.append(self.MarkupLine(s, schema.current))
+
+        schema.Last()
+        result.append(self.MarkupLine(strings[-1], schema.current))
+
+        return result
+
+    """
+    Размечает строку на указанное колено
+    """
+    def MarkupLine (self, line, schema_line):
+        if schema_line  != "1":
+            raise MarkupException('Некорректный номер колена')
+        #конструктор создаем элемент класса дерево
+        tree = SyllableTree(line)
+        #размечаем всё начало строки
+        pointer= tree.first.next.setDown().next.setUp().next.setUp().next.next.setDown()
+        #размечаем последний акцент
+
+        # Если осталось мало слогов?
+        ostatok = tree.count - pointer.num -1
+        if not ostatok:
+            raise MarkupException("Слишком короткая строка: %s" % line)
+        elif ostatok <= 2:
+            pointer = pointer.next.setDown()
+            if pointer != tree.last:
+                pointer.next.setDown()
+        else:
+          ##tree.lastAccent.setLowerAccent()
+          #размечаем последний слог
+          tree.last.setDown()
+
+  #Не разрешать переопределять значки в уже размеченных слогах
+
+        return tree
+
+class Glas8_Schema:
+    def __init__(self):
+        self.current = None
+
+    def Next(self, ostatok):
+        self.current = "1"
+
+    def Last (self):
+        self.current = "1"
+
+
+class OsmoglasieException(Exception):
+    def __init__(self, msg):
+        self.message = msg
+
+class LogicalStringsException(OsmoglasieException):
+    def __init__(self, msg):
+        self.message = msg
+
+class MarkupException(OsmoglasieException):
+    def __init__(self, msg):
+        self.message = msg
+
+
+
+
+
+if __name__ == "__main__":
+    text = """С высоты{0} снизше{0}л еси{0} Благоутро{0}бне,/
+погребе{0}ние прия{0}л еси{0} тридне{0}вное,/
+да на{0}с свободи{0}ши страсте{0}й,//
+Животе{0} и воскресе{0}ние на{0}ше. Гоc{0}поди, сла{0}ва Тебе{0}.
+""".format(chr(0x301) if False else '')
+
+    print(Markup(text, 8, "тропарь"))
---
+++ SyllableTree.py
@@ -0,0 +1,169 @@
+﻿#-------------------------------------------------------------------------------
+# Name:        модуль1
+# Purpose:
+#
+# Author:      Танюшка
+#
+# Created:     29.05.2016
+# Copyright:   (c) Танюшка 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+stressed = chr(0x301)
+
+class SyllableTree:
+    def __init__(self,text):
+        self.first = None
+        self.last = None
+        self.count = None
+
+        self.firstAccent = None
+        self.lastAccent = None
+        self.preLastAccent = None
+
+        #self.BuildWord(text)
+        self.Build(text)
+
+    #построить дерево уровня "слова"
+    def BuildWord(self,text):
+        split = ' ;:.,?!'  # строка символов
+        prev = 0
+        s = []
+        for i in range(len(text)):
+            if text[i] in split:
+                e = text[prev:i]
+                if e != "":
+                    s.append(e)
+                prev =i+1
+            elif (i == len(text)-1):
+                s.append(text[prev:i+1])
+                prev =i+1
+        print(s)
+        return s
+
+    #построить дерево уровня "слоги"
+    def Build(self, text):
+        m = self.SplitToSyllables(text)
+
+        print(m)
+
+        self.syllables = []
+
+        for i in range(len(m)):
+            self.syllables.append(Syllable(m[i], i, isStressed = m[i][-1] == stressed))
+
+        for i in range(len(self.syllables) - 1):
+            self.LinkSyllables(self.syllables[i], self.syllables[i+1])
+
+        self.first = self.syllables[0]
+        self.last = self.syllables[-1]
+
+        self.count = len(self.syllables)
+
+        #self.FindAccents()
+
+    def FindAccents (self):
+        w = self.lastWord
+
+        if w.stressedSyllable == self.last:
+            w = w.prev
+
+        while not w.stressedSyllable:
+            w = w.prev
+
+
+
+
+
+
+
+    #разбить на слоги
+    def SplitToSyllables(self, text):
+        poem = 'аеиоуыэюя'  # строка глассных
+        prev = 0
+        m = []
+        for i in range(len(text)):
+            if text[i] in poem:
+                #< len(text)-1:
+                k = i+1
+                if i+1 < len(text) and text[i+1] == stressed:
+                    k=k+1
+                m.append(text[prev:k])
+                prev = k
+            #elif text[i] in poem and (i+1 == len(text)-1):
+            #    m.append(text[prev:i+2])
+            #    prev =i+1
+        return m
+
+    def LinkSyllables(self, a, b):
+        a.next = b
+        b.prev = a
+
+    def __repr__(self):
+        s = ""
+        for i in self.syllables:
+            s+= i.markup if i.markup else i.str
+
+        return s
+
+
+
+class Syllable:
+    def __init__(self, str, num, next= None, prev = None, isStressed = False):
+        self.str = str
+        self.next = next
+        self.prev = prev
+        #self.isAccent = None
+        self.num = num
+        self.isStressed = isStressed
+        self.markup = None
+
+        # Определим ударение
+        #y = None # значок ударения
+        #if y in self.str:
+        #    self.isStressed = True
+        #else:
+        #    self.isStressed = False
+
+    def CheckMarkup(self):
+        if self.markup:
+            raise SyllableTreeException("Слог '%s' уже размечен" % self.str)
+
+    def setUp(self):
+        self.CheckMarkup()
+        self.markup = self.str+chr(0x301)
+        return self
+
+    def setDown(self):
+        self.CheckMarkup()
+        self.markup = self.str+chr(0x300)
+        return self
+
+    def setLowerAccent(self):
+        self.CheckMarkup()
+        self.markup = self.str+chr(0x30C)
+        return self
+
+    def setUpperAccent(self):
+        self.CheckMarkup()
+        self.markup = self.str+chr(0x302)
+        return self
+
+    def waveAccent(self):
+        self.CheckMarkup()
+        self.markup = self.str+chr(0x305)
+        return self
+
+    def pauseAccent(self):
+        self.CheckMarkup()
+        self.markup = self.str+chr(0x303)
+        return self
+
+def main():
+    a= SyllableTree('С высоты{0} снизше{0}л еси{0} Благоутро{0}бне'.format(stressed))
+
+if __name__ == '__main__':
+    main()
+
+class SyllableTreeException(Exception):
+    def __init__(self, msg):
+        self.message = msg
